ce75bd71140d6aec1021955b583f6e57a2673c68
valikrakots <valllik777@gmail.com>
2021-04-09 18:47:52 +0300

    Refreshments made

--- addHelicopter.sql
+++ addHelicopter.sql
@@ -1,2 +1,2 @@
-INSERT INTO Helicopter (brand, date, capacity, last_overhaul, till_overhaul)
+INSERT INTO Helicopters (brand, date, capacity, last_overhaul, till_overhaul)
     VALUES ('MIG72', '12-02-2012', 53.9, '19-01-2019', 240);
--- addPilot.sql
+++ addPilot.sql
@@ -1,2 +1,2 @@
-INSERT INTO Pilot (surname, position, helicopter_number, experience, adress, birthdate)
+INSERT INTO Pilots (surname, position, helicopter_number, experience, adress, birthdate)
     VALUES ('Petro', 'captain', 1, 14, 'Dereska str., 56', '12-02-1989');
