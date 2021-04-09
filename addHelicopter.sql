fff0014bfd42c775e283e5b2c9b15ff0a65b033b
valikrakots <valllik777@gmail.com>
2021-04-09 18:39:58 +0300

    Add sqls added

---
+++ addHelicopter.sql
@@ -0,0 +1,2 @@
+INSERT INTO Helicopter (brand, date, capacity, last_overhaul, till_overhaul)
+    VALUES ('MIG72', '12-02-2012', 53.9, '19-01-2019', 240);
---
+++ addPilot.sql
@@ -0,0 +1,2 @@
+INSERT INTO Pilot (surname, position, helicopter_number, experience, adress, birthdate)
+    VALUES ('Petro', 'captain', 1, 14, 'Dereska str., 56', '12-02-1989');
