c6c67d8b18ce70ec700c34f5b4d89766be3996be
Herman <herman@spiralarms.org>
2021-04-13 18:15:37 +0300

    Add more helicopter models

--- sql/11-addHelicopter.sql
+++ sql/11-addHelicopter.sql
@@ -1,2 +1,15 @@
 INSERT INTO Helicopters (brand, date, capacity, last_overhaul, till_overhaul, pilot_identification_number)
     VALUES ('MIG72', '12-02-2012', 53.9, '19-01-2019', 240, 119);
+INSERT INTO Helicopters (brand, date, capacity, last_overhaul, till_overhaul, pilot_identification_number)
+    VALUES ('Sikorsky R-4', '16-7-2012', 53.9, '5-1-2006', 240, 119);
+INSERT INTO Helicopters (brand, date, capacity, last_overhaul, till_overhaul, pilot_identification_number)
+    VALUES ('Silvercraft SH-4', '3-6-2012', 53.9, '21-5-2002', 240, 119);
+INSERT INTO Helicopters (brand, date, capacity, last_overhaul, till_overhaul, pilot_identification_number)
+    VALUES ('Yakovlev EG', '20-4-2018', 53.9, '26-1-2005', 240, 119);
+INSERT INTO Helicopters (brand, date, capacity, last_overhaul, till_overhaul, pilot_identification_number)
+    VALUES ('Airmaster H2-B1', '5-10-2018', 53.9, '26-11-2000', 240, 119);
+INSERT INTO Helicopters (brand, date, capacity, last_overhaul, till_overhaul, pilot_identification_number)
+    VALUES ('Bell 201', '11-2-2009', 53.9, '26-11-2018', 240, 119);
+INSERT INTO Helicopters (brand, date, capacity, last_overhaul, till_overhaul, pilot_identification_number)
+    VALUES ('Agusta A103', '28-8-2007', 53.9, '6-6-2015', 240, 119);
+
