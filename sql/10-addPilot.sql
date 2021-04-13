5ccc134ba3f4dc59ca24d53da4a1086a2e029823
Herman <herman@spiralarms.org>
2021-04-13 18:33:11 +0300

    Add more pilots

--- sql/10-addPilot.sql
+++ sql/10-addPilot.sql
@@ -1,2 +1,8 @@
 INSERT INTO Pilots (surname, position, helicopter_number, experience, adress, birthdate)
     VALUES ('Petro', 'captain', 1, 14, 'Dereska str., 56', '12-02-1989');
+INSERT INTO Pilots (surname, position, helicopter_number, experience, adress, birthdate)
+    VALUES ('Vasili', 'pilot', 1, 1, 'International str., 56', '06-10-19');
+INSERT INTO Pilots (surname, position, helicopter_number, experience, adress, birthdate)
+    VALUES ('Matvey', 'pilot', 1, 2, 'Kalinova str., 12', '15-09-1987');
+INSERT INTO Pilots (surname, position, helicopter_number, experience, adress, birthdate)
+    VALUES ('Joseph', 'pilot', 1, 7, 'Bobruyskaya str., 7', '03-03-1993');
