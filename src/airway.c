49d110fde5d1adafa1a9065069163d4260027ce3
Herman <herman@spiralarms.org>
2021-04-13 22:51:39 +0300

    Add more queries
    Define more constants
    
    Minor changes

--- src/airway.c
+++ src/airway.c
@@ -3,7 +3,16 @@
 #include <string.h>
 #include <sqlite3.h>
 
-#define QUERY1 "SELECT * FROM FLIGHTS"
+#define QUERY_ALL_FLIGHTS "SELECT * FROM Flights"
+#define Q_ALL_FLIGHTS	'F'
+#define QUERY_ALL_HELICOPTERS "SELECT * FROM Helicopters"
+#define Q_ALL_HELICOPTERS	'H'
+#define QUERY_ALL_PILOTS "SELECT * FROM Pilots"
+#define Q_ALL_PILOTS	'P'
+#define QUERY_1 "SELECT Helicopters.brand, sum(Flights.hours) FROM Helicopters INNER JOIN Flights ON Helicopters.number = Flights.helicopter_number WHERE Helicopters.last_overhaul < Flights.date;"
+#define Q_1 'o'
+#define QUERY_2 "SELECT Helicopters.brand, sum(Flights.hours), sum(Flights.quantity), Flights.id, Flights.date FROM Helicopters INNER JOIN Flights ON Helicopters.number = Flights.helicopter_number;"
+#define Q_2 'f'
 
 void print_greeting();
 sqlite3* connect_to_database(char *filename);
@@ -34,9 +43,11 @@ char
 ask_query()
 {
 	printf("q\tquit\n");
-	printf("0\tquery1\n");
-	printf("1\tquery2\n");
-	printf("2\tquery3\n");
+	printf("%c\t%s\n", Q_ALL_HELICOPTERS, QUERY_ALL_HELICOPTERS);
+	printf("%c\t%s\n", Q_ALL_PILOTS, QUERY_ALL_PILOTS);
+	printf("%c\t%s\n", Q_ALL_FLIGHTS, QUERY_ALL_FLIGHTS);
+	printf("%c\t%s\n", Q_1, QUERY_1);
+	printf("%c\t%s\n", Q_2, QUERY_2);
 	printf("\n");
 	printf("\033[32m");
 	system("/bin/stty raw");
@@ -77,17 +88,27 @@ int main(int argc, char **argv)
 		case 'q':
 			done = 1;
 			break;
-		case '0':
-			strcpy(query, QUERY1);
+		case Q_ALL_HELICOPTERS:
+			strcpy(query, QUERY_ALL_HELICOPTERS);
 			break;
-		case '1': /* FALLTHROUGH */
-		case '2': /* FALLTHROUGH */
-			strcpy(query, "\0");
+		case Q_ALL_PILOTS:
+			strcpy(query, QUERY_ALL_PILOTS);
+			break;
+		case Q_ALL_FLIGHTS:
+			strcpy(query, QUERY_ALL_FLIGHTS);
+			break;
+		case Q_1:
+			strcpy(query, QUERY_1);
+			break;
+		case Q_2:
+			strcpy(query, QUERY_2);
 			break;
 		default:
 			printf("\033[33mᐱ unknown query\033[0m\n", qn);
+			continue;
 		}
 		if (done) break;
+		printf("\n");
 		int rc = sqlite3_exec(db, query, callback, NULL, NULL);
 		char *zErrMsg;
 		if (rc != SQLITE_OK) {
